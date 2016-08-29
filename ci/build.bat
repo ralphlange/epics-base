38fd6ec05363f9e023cf37f62ac897fe3f3f0ca9
Ralph Lange <ralph.lange@gmx.de>
2016-08-29 17:12:25 +0200

    Next version...

--- ci/build.bat
+++ ci/build.bat
@@ -1,11 +1,15 @@
 :: Universal build script for AppVeyor (https://ci.appveyor.com/)
 :: Environment:
-::             STATIC   -   YES/NO   determines EPICS build
+::     TOOLCHAIN_VERSION   -  Visual Studio Version
+::     STATIC     YES/NO   -  determines EPICS build
 ::
 :: All command line args are passed to make
 
+set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio %TOOLCHAIN_VERSION%\VC\vcvarsall.bat"
+
 set "ST="
 if /i "%STATIC%"=="YES" set ST=-static
+
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 if "%OS%"=="64BIT" (
     set EPICS_HOST_ARCH=windows-x64%ST%
@@ -14,13 +18,11 @@ if "%OS%"=="64BIT" (
     set EPICS_HOST_ARCH=win32-x86%ST%
     set COMPILER=x86
 )
-for /l %%X in (14,-1,8) do (
-    if exist "C:\Program Files (x86)\Microsoft Visual Studio %%X.0\VC\vcvarsall.bat" (
-        "C:\Program Files (x86)\Microsoft Visual Studio %%X.0\VC\vcvarsall.bat" %COMPILER%
-        echo [INFO] Using Microsoft Visual Studio %%X.0
-        goto :BUILD
-    )
-)
-:BUILD
-echo [INFO] EPICS_HOST_ARCH = %EPICS_HOST_ARCH%
+call %VCVARSALL% %COMPILER%
+if %ERRORLEVEL% NEQ 0 exit 1
+
+echo [INFO] Microsoft Visual Studio Version %TOOLCHAIN_VERSION%
+echo [INFO] Platform: %COMPILER%
+echo [INFO] EPICS_HOST_ARCH: %EPICS_HOST_ARCH%
+
 C:\MinGW\bin\mingw32-make %*
