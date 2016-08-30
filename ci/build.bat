e12cfe28c2e111743d73deff46c48e7afba0a4f5
Ralph Lange <ralph.lange@gmx.de>
2016-08-30 14:36:03 +0200

    full matrix; only use scripts for setup

--- ci/build.bat
+++ ci/build.bat
@@ -14,6 +14,7 @@ if "%PLATFORM%"=="x86" set OS=32BIT
 echo [INFO] Platform: %OS%
 
 if "%TOOLCHAIN%"=="cygwin" (
+    if "%CONFIGURATION%"=="static" exit 1
     if "%OS%"=="64BIT" (
         set EPICS_HOST_ARCH=cygwin-x86_64
         set "INCLUDE=C:\cygwin64\include;%INCLUDE%"
@@ -30,6 +31,7 @@ if "%TOOLCHAIN%"=="cygwin" (
 )
 
 if "%TOOLCHAIN%"=="mingw" (
+    if "%CONFIGURATION%"=="static" exit 1
     if "%OS%"=="64BIT" (
         set EPICS_HOST_ARCH=windows-x64-mingw
         set "INCLUDE=C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\include;%INCLUDE%"
@@ -49,25 +51,33 @@ set "VSINSTALL=C:\Program Files (x86)\Microsoft Visual Studio %TOOLCHAIN%"
 
 if "%OS%"=="64BIT" (
     set EPICS_HOST_ARCH=windows-x64%ST%
+    if exist "%VSINSTALL%\VC\vcvarsall.bat" (
+        call "%VSINSTALL%\VC\vcvarsall.bat" amd64
+        goto MSFound
+    )    
     if exist "%VSINSTALL%\VC\bin\amd64\vcvars64.bat" (
         call "%VSINSTALL%\VC\bin\amd64\vcvars64.bat"
-    ) else (
-        set "INCLUDE=%VSINSTALL%\VC\include;%INCLUDE%"
-        set "PATH=%VSINSTALL%\VC\bin\amd64;%PATH%"
-        set "LIB=%VSINSTALL%\VC\lib\amd64;%LIB%"
-        set "LIBPATH=%VSINSTALL%\VC\lib\amd64;%LIBPATH%"
+        goto MSFound
     )
 ) else (
     set EPICS_HOST_ARCH=win32-x86%ST%
+    if exist "%VSINSTALL%\VC\vcvarsall.bat" (
+        call "%VSINSTALL%\VC\vcvarsall.bat" x86
+        goto MSFound
+    )    
+    if exist "%VSINSTALL%\VC\bin\vcvars32.bat" (
+        call "%VSINSTALL%\VC\bin\vcvars32.bat"
+        goto MSFound
+    )
     if exist "%VSINSTALL%\Common7\Tools\vsvars32.bat" (
         call "%VSINSTALL%\Common7\Tools\vsvars32.bat"
-    ) else (
-        set "INCLUDE=%VSINSTALL%\VC\include;%INCLUDE%"
-        set "PATH=%VSINSTALL%\VC\bin;%PATH%"
-        set "LIB=%VSINSTALL%\VC\lib;%LIB%"
-        set "LIBPATH=%VSINSTALL%\VC\lib;%LIBPATH%"
+        goto MSFound
     )
 )
+echo [INFO] Installation for MSVC Toolchain %TOOLCHAIN% / %OS% seems to be missing
+exit 1
+
+:MSFound
 echo [INFO] Microsoft Visual Studio Toolchain %TOOLCHAIN%
 echo [INFO] Compiler Version
 cl
