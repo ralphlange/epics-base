f6e72c06098c886b1b46e93f6c431744f72219aa
Ralph Lange <ralph.lange@gmx.de>
2016-09-01 14:21:11 +0200

    next test

--- ci/appveyor_make.bat
+++ ci/appveyor_make.bat
@@ -16,14 +16,19 @@ if "%PLATFORM%"=="x86" set OS=32BIT
 echo [INFO] Platform: %OS%
 
 if "%TOOLCHAIN%"=="cygwin" (
+    set "MAKE=make"
     if "%OS%"=="64BIT" (
         set EPICS_HOST_ARCH=cygwin-x86_64
         set "INCLUDE=C:\cygwin64\include;%INCLUDE%"
         set "PATH=C:\cygwin64\bin;%PATH%"
+        echo [INFO] Cygwin Toolchain 64bit
+        dir C:\cygwin64\bin
     ) else (
         set EPICS_HOST_ARCH=cygwin-x86
         set "INCLUDE=C:\cygwin\include;%INCLUDE%"
         set "PATH=C:\cygwin\bin;%PATH%"
+        echo [INFO] Cygwin Toolchain 32bit
+        dir C:\cygwin\bin
     )
     echo [INFO] Cygwin Toolchain
     echo [INFO] Compiler Version
